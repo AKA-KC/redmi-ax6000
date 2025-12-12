51173b80d58dd4e8c90240309589d8f84efca709
K.C Wong <4024715+AKA-KC@users.noreply.github.com>
2025-12-12 23:15:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,12 @@
 # 你需要的 Nikki, Tailscale, DDNS-Go 等插件
 # 我们已经全部安排在 diy-part2.sh 里处理了。
 # 这个文件留空或者保持默认即可，不需要添加任何代码。
+
+#!/bin/bash
+# File name: diy-part1.sh
+# description: 添加第三方插件源 (Kenzok8)
+
+# 添加 kenzok8 的 small 仓库 (包含 Nikki, TurboACC, Golang 等全套依赖)
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# 添加依赖库
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
